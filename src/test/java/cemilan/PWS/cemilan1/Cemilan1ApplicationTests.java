5ba476846ebc523605f5586f85fc43088477bc88
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2022-10-18 19:44:52 +0700

    build project

---
+++ src/test/java/cemilan/PWS/cemilan1/Cemilan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cemilan.PWS.cemilan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cemilan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
